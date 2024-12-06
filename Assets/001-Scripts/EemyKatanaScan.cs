757ba0a254dc56fef06c9e9b923dbeef7523ba61
taiyaki-jp <ttc2350dg0001@edu.tech.ac.jp>
2024-12-06 16:11:39 +0900

    解析を開始

--- Assets/001-Scripts/EemyKatanaScan.cs
+++ Assets/001-Scripts/EemyKatanaScan.cs
@@ -1,16 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EemyKatanaScan : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         // trQ Å® v3Angle
--- Assets/001-Scripts/ObjectAddForce.cs
+++ Assets/001-Scripts/ObjectAddForce.cs
@@ -1,21 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectAddForce : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         // ‰“‚­‚É”ò‚Î‚·
         Rigidbody rb = GetComponent<Rigidbody>();
         Vector3 force = new Vector3(0.0f, 0.0f, -5.0f);
         rb.AddForce(force, ForceMode.Impulse);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
